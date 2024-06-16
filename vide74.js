f15a1e53fcf4b4d3085e5d656711a359edadbf3a
Bablu <gautamprince404@gmail.com>
2024-06-16 11:42:34 +0530

    update

--- vide74.js
+++ vide74.js
@@ -9,3 +9,38 @@
 // button.addEventListener("contextmenu",()=>{
 // alert("Right click please here")
 // })
+
+document.querySelector(".child").addEventListener("click", (e) => {
+    e.stopPropagation()
+    alert("child was clicked");
+  });
+  document.querySelector(".childchildren")
+    .addEventListener("click", (e) => {
+      e.stopPropagation()
+      alert("childChildren Was clicked");
+    });
+
+  document.querySelector(".container").addEventListener("click", (e) => {
+    alert("container Was clicked");
+  });
+
+  
+  
+  
+  function getRandomColor() {
+    let val1 = Math.ceil(0 + Math.random() * 255);
+    let val2 = Math.ceil(0 + Math.random() * 255);
+    let val3 = Math.ceil(0 + Math.random() * 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+// let a = setInterval(() => {
+//   document.querySelector(".childchildren").style.background= getRandomColor();
+  
+// }, 1000);
+// console.log(a)
+
+let a = setTimeout(() => {
+  document.querySelector(".childchildren").style.background= getRandomColor();
+  
+}, 3000);
+console.log(a)
