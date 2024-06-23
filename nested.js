78cee7f6d9184b16b7d84cd65a49ca4ae01a4c15
Bablu <gautamprince404@gmail.com>
2024-06-24 02:26:22 +0530

    javascript |||

---
+++ nested.js
@@ -0,0 +1,41 @@
+// bag="";
+// for(i=1; i<=5; i++){
+//     // bag="";
+
+//     for(j=1; j<=5; i++){
+
+//         bag=bag+"*"+" ";
+
+//     }
+//     console.log(bag)
+// }
+
+
+// study of function //
+
+// function name(){   // decleration a function
+//     // execute code //
+
+// }
+// calling a function name() //
+
+// function call(name){
+//     console.log("please call", name)
+// }
+// call("Bablu")
+
+// function call(name,name1){
+//         console.log("please call", name,name1)
+//     }
+//     call("Bablu", "Aman")  // we call many function at a time
+
+
+ function call(a,b){
+           let sum=a+b;
+           console.log(2*sum)//
+        // why we use return function//
+        // return sum;
+        }
+    //    let ans= call(2,8) 
+    //     console.log(2*ans)
+    call(2,8)
